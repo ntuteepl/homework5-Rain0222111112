f1ac792d908578418d9a00289c8a5dc26971925b
112310222 <146065193+Rain0222111112@users.noreply.github.com>
2023-10-31 14:35:26 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int input;
+    double count ;
+    scanf("%d" , &input);
+    if (input <= 800){
+        count = input *0.9;
+    }
+    else if(input > 800 && input < 1500){
+        count = input *0.81;
+    }
+    else {
+        count = input *0.9*0.79;
+    }
+    printf("%0.1f\n" , count);
+}
 
